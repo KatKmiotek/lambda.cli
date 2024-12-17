bd07c1e63749e220123a59aa912a8aa31c94f970
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2024-12-17 11:12:59 +0000

    feat: refactor lots

--- README.md
+++ README.md
@@ -18,3 +18,33 @@ provide:
 - name for your project
 - select language ( TypeScript, Dotnet, Python)
 - select if Terraform file for new lambda needs to be created (output directory ./terraform/)
+
+
+## Local development
+
+Tutorials:
+1. **[Running project locally](#tutorial-1-local-setup)**
+2. **[Installing pre-commit hooks](#tutorial-2-installing-pre-commit-hooks)**
+
+---
+
+### Tutorial 1: Running project locally
+
+#### Objective
+By following those steps you will setup your machine to run CLI Docs tool
+
+Steps:
+1. **Step 1**: Mac - Install rustup via [instruction](https://doc.rust-lang.org/cargo/getting-started/) from The Rust book
+2. **Step 2**: Windows - Install rustup via [instruction](https://doc.rust-lang.org/cargo/getting-started/installation.html) from The Rust book
+3. **Step 3**: Confirm `cargo -V` outputs version 1.82.0 or higher
+4. **Step 4**: Run `cargo run` - this will build project
+
+### Tutorial 2: Installing pre-commit hooks
+
+#### Objective
+Execute static code analysis locally
+
+Steps:
+1. **Step 1**: Install [pre-commit]()
+2. **Step 2**: Run `pre-commit install` to add hook to .git/hooks/pre-commit - from now on git commit event staged files will be checked
+3. **Step 3**: To run pre-commit on all files `pre-commit run --all-files`
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::{error::Error, fs, path::Path, process::Command};
-
 use clap::{Parser, Subcommand};
 use dialoguer::{theme::ColorfulTheme, Input, Select};
 use handlebars::Handlebars;
 use serde_json::json;
+use std::{error::Error, fs, path::Path, process::Command};
 
 #[derive(Parser)]
 #[command(name = "template")]
@@ -18,12 +17,40 @@ enum Commands {
     Lambda {},
 }
 
-fn load_templates(runtime: &str, context: &str) -> Result<Handlebars<'static>, Box<dyn Error>> {
+#[derive(Debug, Clone, Copy)]
+enum Runtime {
+    TypeScript,
+    Dotnet,
+    Python,
+}
+
+impl Runtime {
+    fn runtime_to_lowercase(&self) -> String {
+        match self {
+            Runtime::TypeScript => "typescript",
+            Runtime::Dotnet => "dotnet",
+            Runtime::Python => "python",
+        }
+        .to_string()
+    }
+}
+
+impl std::fmt::Display for Runtime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Runtime::TypeScript => write!(f, "TypeScript"),
+            Runtime::Dotnet => write!(f, "Dotnet"),
+            Runtime::Python => write!(f, "Python"),
+        }
+    }
+}
+
+fn load_templates(runtime: Runtime, context: &str) -> Result<Handlebars<'static>, Box<dyn Error>> {
     let mut handlebars = Handlebars::new();
 
     let template_dir_name = format!("{}_templates", context);
-    let template_dir = Path::new(template_dir_name.as_str()).join(runtime.to_lowercase());
-    for entry in fs::read_dir(&template_dir).unwrap() {
+    let template_dir = Path::new(&template_dir_name).join(runtime.runtime_to_lowercase());
+    for entry in fs::read_dir(&template_dir).expect("Directory doesn't exist") {
         let entry = entry?;
         let path = entry.path();
 
@@ -42,49 +69,8 @@ fn load_templates(runtime: &str, context: &str) -> Result<Handlebars<'static>, B
     Ok(handlebars)
 }
 
-fn create_lambda_files(name: &str, runtime: &str) -> Result<String, Box<dyn Error>> {
-    let handlebars = load_templates(runtime, "lambda")?;
-
-    let data = json!({
-        "project_name": name,
-        "runtime": runtime,
-    });
-    let project_dir_name = format!("{}/src", name);
-
-    let project_dir = Path::new(project_dir_name.as_str());
-    if !project_dir.exists() {
-        fs::create_dir_all(project_dir)?;
-    }
-
-    let template_dir = Path::new("lambda_templates").join(runtime.to_lowercase());
-
-    for entry in fs::read_dir(template_dir).unwrap() {
-        let entry = entry?;
-        let template_path = entry.path();
-
-        if template_path.is_file() {
-            let template_name = template_path
-                .file_stem()
-                .and_then(|n| n.to_str())
-                .ok_or("Invalid template filename")?;
-
-            let output_filename = template_path
-                .file_name()
-                .and_then(|n| n.to_str())
-                .ok_or("Invalid output filename")?;
-
-            let content = handlebars.render(template_name, &data)?;
-
-            let output_path = project_dir.join(output_filename);
-            fs::write(output_path, content)?;
-        }
-    }
-
-    Ok(project_dir_name)
-}
-
-fn run_post_creation_commands(project_name: &str, runtime: &str) -> Result<(), Box<dyn Error>> {
-    if runtime == "TypeScript" {
+fn run_post_creation_commands(project_name: &str, runtime: Runtime) -> Result<(), Box<dyn Error>> {
+    if let Runtime::TypeScript = runtime {
         println!("Installing npm dependencies...");
         let status = Command::new("npm")
             .current_dir(project_name)
@@ -98,20 +84,51 @@ fn run_post_creation_commands(project_name: &str, runtime: &str) -> Result<(), B
     Ok(())
 }
 
-fn create_terraform_files(name: &String, runtime: &str) -> Result<(), Box<dyn Error>> {
-    let handlebars = load_templates(runtime, "terraform").unwrap();
+fn create_dotnet_lambda(name: &str) -> Result<String, Box<dyn Error>> {
+    println!("Installing .NET tools...");
+    Command::new("dotnet")
+        .arg("new")
+        .arg("install")
+        .arg("Amazon.Lambda.Templates")
+        .status()
+        .expect("Failed to install templates");
+
+    println!("Creating .NET Lambda project...");
+    Command::new("dotnet")
+        .arg("new")
+        .arg("lambda.EmptyFunction")
+        .arg("--name")
+        .arg(name)
+        .status()
+        .expect("Failed to create .NET Lambda project");
+    Ok(name.to_string())
+}
+
+fn create_project_files(
+    name: &String,
+    runtime: Runtime,
+    context: &str,
+) -> Result<String, Box<dyn Error>> {
+    let handlebars = load_templates(runtime, context).unwrap();
     let data = json!({
         "project_name": name,
-        "runtime": runtime,
+        "runtime": runtime.to_string(),
     });
-    let project_dir = Path::new("terraform");
+
+    let project_dir_name = match context {
+        "terraform" => "terraform".to_string(),
+        "lambda" => format!("{}/src", name),
+        _ => todo!(),
+    };
+    let project_dir = Path::new(&project_dir_name);
     if !project_dir.exists() {
         fs::create_dir_all(project_dir)?;
     }
 
-    let template_dir = Path::new("terraform_templates").join(runtime.to_lowercase());
+    let template_dir =
+        Path::new(&format!("{}_templates", context)).join(runtime.runtime_to_lowercase());
 
-    for entry in fs::read_dir(template_dir).unwrap() {
+    for entry in fs::read_dir(template_dir).expect("Directory doesn't exist") {
         let entry = entry?;
         let template_path = entry.path();
 
@@ -129,11 +146,12 @@ fn create_terraform_files(name: &String, runtime: &str) -> Result<(), Box<dyn Er
             let content = handlebars.render(template_name, &data)?;
 
             let output_path = project_dir.join(output_filename);
-            fs::write(output_path, content)?;
+            fs::write(output_path, content)?
         }
     }
-    Ok(())
+    Ok(project_dir_name)
 }
+
 fn main() {
     Cli::parse();
     let name: String = Input::with_theme(&ColorfulTheme::default())
@@ -141,7 +159,7 @@ fn main() {
         .interact_text()
         .unwrap();
 
-    let runtimes = &["TypeScript", "Dotnet", "Python"];
+    let runtimes = &[Runtime::TypeScript, Runtime::Dotnet, Runtime::Python];
 
     let runtime = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Select runtime")
@@ -157,40 +175,52 @@ fn main() {
         .items(&yes_no[..])
         .interact()
         .unwrap();
-    let project_directory = match runtimes[runtime] {
-        "Dotnet" => {
-            println!("Installing .NET tools...");
-            Command::new("dotnet")
-                .arg("new")
-                .arg("install")
-                .arg("Amazon.Lambda.Templates")
-                .status()
-                .expect("Failed to install templates");
 
-            println!("Creating .NET Lambda project...");
-            Command::new("dotnet")
-                .arg("new")
-                .arg("lambda.EmptyFunction")
-                .arg("--name")
-                .arg(&name)
-                .status()
-                .expect("Failed to create .NET Lambda project");
-            name.clone()
+    let project_directory = match runtimes[runtime] {
+        Runtime::Dotnet => create_dotnet_lambda(&name).unwrap(),
+        Runtime::Python | Runtime::TypeScript => {
+            create_project_files(&name, runtimes[runtime], "lambda").unwrap()
         }
-        "Python" | "TypeScript" => create_lambda_files(&name, runtimes[runtime]).unwrap(),
-        _ => todo!(),
     };
+
     run_post_creation_commands(&project_directory, runtimes[runtime]).unwrap();
+
     match need_terraform {
-        0 => create_terraform_files(&name, runtimes[runtime]).unwrap(),
-        1 => {
-            println!("No terraform will be created")
+        0 => {
+            create_project_files(&name, runtimes[runtime], "terraform").unwrap();
         }
-        _ => todo!(),
+        _ => println!("No terraform will be created"),
     }
 
     println!(
-        "Your {} lambda in {} has been created",
-        name, runtimes[runtime]
+        "Your {} lambda in {:?} has been created",
+        name,
+        runtimes[runtime].to_string()
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_runtime_conversions() {
+        let runtime = Runtime::TypeScript;
+        assert_eq!(runtime.runtime_to_lowercase(), "typescript");
+        assert_eq!(runtime.to_string(), "TypeScript");
+
+        let runtime = Runtime::Python;
+        assert_eq!(runtime.runtime_to_lowercase(), "python");
+        assert_eq!(runtime.to_string(), "Python");
+    }
+
+    // #[test]
+    // fn test_error_cases() {
+    //     let result = load_templates(Runtime::TypeScript, "nonexistent");
+    //     assert!(result.is_err());
+
+    //     let result =
+    //         create_project_files(&String::from(""), Runtime::TypeScript, "invalid_context");
+    //     assert!(result.is_err());
+    // }
 }

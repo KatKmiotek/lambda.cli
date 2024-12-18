da2eaf26cb87dc879e154064d04d55638e7b7f2d
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2024-12-18 13:51:36 +0000

    feat: update tf project default

--- src/terraform.rs
+++ src/terraform.rs
@@ -7,7 +7,7 @@ use crate::{runtime::Runtime, template_helper::create_project_files};
 pub fn create_terraform_project() -> Result<(), Box<dyn Error>> {
     let name: String = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Project name")
-        .default("./terraform".to_string())
+        .default("terraform".to_string())
         .interact_text()
         .unwrap();
 
